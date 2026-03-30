21868d6f0fa51180b0cd719a7997b7c99c4c0af4
calimits <calimitsacd@gmail.com>
2026-03-29 21:41:17 -0400

    feat: docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieparser from "cookie-parser"
+import cookieparser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "node:path";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   app.use(cookieparser());
   await app.listen(process.env.PORT ?? 3000);
 }
