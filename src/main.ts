175fc0e36d62e50a90e355796fa6e38c7ce607e4
calimits <calimitsacd@gmail.com>
2026-03-30 14:54:09 -0400

    feat: added security

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,25 @@ import cookieparser from "cookie-parser";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import path from "node:path";
+import helmet from "helmet";
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
   app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-  console.log(process.env.AUTH_SERVICE_URL);
+  
+  //security
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, 
+      max: 200, 
+      standardHeaders: true, 
+      legacyHeaders: false, 
+    }),
+  );
+
   app.use(cookieparser());
   await app.listen(process.env.PORT ?? 3000);
 }
